03a2e0709ab20429ef882e5eedf80297a77463ac
KESOUATI <kesouatioualid@gmail.com>
2025-03-10 00:56:46 +0000

    first commit for opsidium

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"product-list",component:ProductlistComponent},
+  {path:"add-product",component:AddProductComponent},
+  {path:"", redirectTo :"product-list",pathMatch : 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
